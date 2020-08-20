9af1d0f2e05ffd1fe45d19707358bc319c12d497
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-20 20:50:37 +1000

    Refactor colour handling logic

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ const NOEL: Theme = Theme {
     secondary_accent2: Rgb(0xEEB9C1),
 };
 
-const INVISIBLE: Rgba = Rgba { rgb: Rgb(0x000000), a: 0x00 };
+const INVISIBLE: Color = Color { rgb: Rgb(0x000000), a: Some(0x00) };
 
 struct Theme {
     name: &'static str,
@@ -40,111 +40,111 @@ impl Theme {
 
         write_scope(f, "activityBar.background", self.darker_bg)?;
         write_scope(f, "activityBar.foreground", self.fg)?;
-        write_scope(f, "activityBar.inactiveForeground", Rgba { rgb: self.primary_accent, a: 0x44 })?;
+        write_scope(f, "activityBar.inactiveForeground", (self.primary_accent, 0x44))?;
         write_scope(f, "activityBarBadge.background", self.primary_accent)?;
         write_scope(f, "activityBarBadge.foreground", self.darker_bg)?;
-        write_scope(f, "badge.background", Rgba { rgb: self.primary_accent, a: 0x33 })?;
+        write_scope(f, "badge.background", (self.primary_accent, 0x33))?;
         write_scope(f, "debugToolBar.background", self.darker_bg)?;
-        write_scope(f, "diffEditor.insertedTextBackground", Rgba { rgb: self.green, a: 0x22 })?;
-        write_scope(f, "diffEditor.removedTextBackground", Rgba { rgb: self.red, a: 0x22 })?;
+        write_scope(f, "diffEditor.insertedTextBackground", (self.green, 0x22))?;
+        write_scope(f, "diffEditor.removedTextBackground", (self.red, 0x22))?;
         write_scope(f, "dropdown.background", self.darker_bg)?;
         write_scope(f, "dropdown.foreground", self.fg)?;
         write_scope(f, "editor.background", self.bg)?;
-        write_scope(f, "editor.findMatchBackground", Rgba { rgb: self.secondary_accent1, a: 0x77 })?;
-        write_scope(f, "editor.findMatchHighlightBackground", Rgba { rgb: self.secondary_accent1, a: 0x44 })?;
+        write_scope(f, "editor.findMatchBackground", (self.secondary_accent1, 0x77))?;
+        write_scope(f, "editor.findMatchHighlightBackground", (self.secondary_accent1, 0x44))?;
         write_scope(f, "editor.foreground", self.fg)?;
-        write_scope(f, "editor.lineHighlightBackground", Rgba { rgb: self.primary_accent, a: 0x0D })?;
-        write_scope(f, "editor.rangeHighlightBackground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
-        write_scope(f, "editor.selectionBackground", Rgba { rgb: self.primary_accent, a: 0x33 })?;
-        write_scope(f, "editor.wordHighlightBackground", Rgba { rgb: self.primary_accent, a: 0x33 })?;
-        write_scope(f, "editorCodeLens.foreground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "editor.lineHighlightBackground", (self.primary_accent, 0x0D))?;
+        write_scope(f, "editor.rangeHighlightBackground", (self.primary_accent, 0x22))?;
+        write_scope(f, "editor.selectionBackground", (self.primary_accent, 0x33))?;
+        write_scope(f, "editor.wordHighlightBackground", (self.primary_accent, 0x33))?;
+        write_scope(f, "editorCodeLens.foreground", (self.primary_accent, 0x55))?;
         write_scope(f, "editorCursor.foreground", self.fg)?;
         write_scope(f, "editorError.foreground", self.red)?;
-        write_scope(f, "editorGroup.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "editorGroup.border", (self.primary_accent, 0x14))?;
         write_scope(f, "editorGroupHeader.noTabsBackground", self.darker_bg)?;
         write_scope(f, "editorGroupHeader.tabsBackground", self.darker_bg)?;
         write_scope(f, "editorGutter.addedBackground", self.green)?;
         write_scope(f, "editorGutter.deletedBackground", self.red)?;
         write_scope(f, "editorGutter.modifiedBackground", self.orange)?;
         write_scope(f, "editorHoverWidget.background", self.darker_bg)?;
-        write_scope(f, "editorIndentGuide.activeBackground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
-        write_scope(f, "editorIndentGuide.background", Rgba { rgb: self.primary_accent, a: 0x11 })?;
+        write_scope(f, "editorIndentGuide.activeBackground", (self.primary_accent, 0x22))?;
+        write_scope(f, "editorIndentGuide.background", (self.primary_accent, 0x11))?;
         write_scope(f, "editorLineNumber.activeForeground", self.fg)?;
-        write_scope(f, "editorLineNumber.foreground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "editorLineNumber.foreground", (self.primary_accent, 0x55))?;
         write_scope(f, "editorLink.activeForeground", self.secondary_accent1)?;
         write_scope(f, "editorOverviewRuler.addedForeground", self.green)?;
         write_scope(f, "editorOverviewRuler.deletedForeground", self.red)?;
         write_scope(f, "editorOverviewRuler.errorForeground", self.red)?;
-        write_scope(f, "editorOverviewRuler.findMatchForeground", Rgba { rgb: self.secondary_accent1, a: 0x77 })?;
+        write_scope(f, "editorOverviewRuler.findMatchForeground", (self.secondary_accent1, 0x77))?;
         write_scope(f, "editorOverviewRuler.modifiedForeground", self.orange)?;
-        write_scope(f, "editorOverviewRuler.rangeHighlightForeground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
+        write_scope(f, "editorOverviewRuler.rangeHighlightForeground", (self.primary_accent, 0x22))?;
         write_scope(f, "editorOverviewRuler.warningForeground", self.orange)?;
-        write_scope(f, "editorRuler.foreground", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "editorRuler.foreground", (self.primary_accent, 0x14))?;
         write_scope(f, "editorWarning.foreground", self.orange)?;
-        write_scope(f, "editorWhitespace.foreground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
+        write_scope(f, "editorWhitespace.foreground", (self.primary_accent, 0x22))?;
         write_scope(f, "editorWidget.background", self.darker_bg)?;
-        write_scope(f, "editorWidget.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "editorWidget.border", (self.primary_accent, 0x14))?;
         write_scope(f, "errorForeground", self.red)?;
         write_scope(f, "focusBorder", INVISIBLE)?;
         write_scope(f, "foreground", self.fg)?;
         write_scope(f, "gitDecoration.addedResourceForeground", self.green)?;
         write_scope(f, "gitDecoration.deletedResourceForeground", self.red)?;
         write_scope(f, "gitDecoration.modifiedResourceForeground", self.orange)?;
-        write_scope(f, "input.background", Rgba { rgb: self.primary_accent, a: 0x11 })?;
-        write_scope(f, "input.placeholderForeground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
-        write_scope(f, "list.activeSelectionBackground", Rgba { rgb: self.primary_accent, a: 0x33 })?;
+        write_scope(f, "input.background", (self.primary_accent, 0x11))?;
+        write_scope(f, "input.placeholderForeground", (self.primary_accent, 0x55))?;
+        write_scope(f, "list.activeSelectionBackground", (self.primary_accent, 0x33))?;
         write_scope(f, "list.activeSelectionForeground", self.fg)?;
         write_scope(f, "list.errorForeground", self.red)?;
-        write_scope(f, "list.focusBackground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
+        write_scope(f, "list.focusBackground", (self.primary_accent, 0x22))?;
         write_scope(f, "list.highlightForeground", self.secondary_accent1)?;
-        write_scope(f, "list.hoverBackground", Rgba { rgb: self.primary_accent, a: 0x11 })?;
-        write_scope(f, "list.inactiveSelectionBackground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
+        write_scope(f, "list.hoverBackground", (self.primary_accent, 0x11))?;
+        write_scope(f, "list.inactiveSelectionBackground", (self.primary_accent, 0x22))?;
         write_scope(f, "list.warningForeground", self.orange)?;
         write_scope(f, "menu.background", self.darker_bg)?;
         write_scope(f, "minimap.errorHighlight", self.red)?;
-        write_scope(f, "minimap.findMatchHighlight", Rgba { rgb: self.secondary_accent1, a: 0x77 })?;
-        write_scope(f, "minimap.selectionHighlight", Rgba { rgb: self.primary_accent, a: 0x11 })?;
+        write_scope(f, "minimap.findMatchHighlight", (self.secondary_accent1, 0x77))?;
+        write_scope(f, "minimap.selectionHighlight", (self.primary_accent, 0x11))?;
         write_scope(f, "minimap.warningHighlight", self.orange)?;
         write_scope(f, "minimapGutter.addedBackground", self.green)?;
         write_scope(f, "minimapGutter.deletedBackground", self.red)?;
         write_scope(f, "minimapGutter.modifiedBackground", self.orange)?;
-        write_scope(f, "minimapSlider.activeBackground", Rgba { rgb: self.primary_accent, a: 0x33 })?;
-        write_scope(f, "minimapSlider.background", Rgba { rgb: self.primary_accent, a: 0x11 })?;
-        write_scope(f, "minimapSlider.hoverBackground", Rgba { rgb: self.primary_accent, a: 0x22 })?;
-        write_scope(f, "panel.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "minimapSlider.activeBackground", (self.primary_accent, 0x33))?;
+        write_scope(f, "minimapSlider.background", (self.primary_accent, 0x11))?;
+        write_scope(f, "minimapSlider.hoverBackground", (self.primary_accent, 0x22))?;
+        write_scope(f, "panel.border", (self.primary_accent, 0x14))?;
         write_scope(f, "panelTitle.activeForeground", self.fg)?;
-        write_scope(f, "panelTitle.inactiveForeground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "panelTitle.inactiveForeground", (self.primary_accent, 0x55))?;
         write_scope(f, "peekView.border", self.darker_bg)?;
         write_scope(f, "peekViewEditor.background", self.darker_bg)?;
-        write_scope(f, "peekViewEditor.matchHighlightBackground", Rgba { rgb: self.secondary_accent1, a: 0x77 })?;
+        write_scope(f, "peekViewEditor.matchHighlightBackground", (self.secondary_accent1, 0x77))?;
         write_scope(f, "peekViewResult.background", self.darker_bg)?;
         write_scope(f, "peekViewResult.fileForeground", self.fg)?;
-        write_scope(f, "peekViewResult.lineForeground", Rgba { rgb: self.fg, a: 0xBB })?;
-        write_scope(f, "peekViewResult.matchHighlightBackground", Rgba { rgb: self.secondary_accent1, a: 0x77 })?;
-        write_scope(f, "peekViewResult.selectionBackground", Rgba { rgb: self.primary_accent, a: 0x33 })?;
+        write_scope(f, "peekViewResult.lineForeground", (self.fg, 0xBB))?;
+        write_scope(f, "peekViewResult.matchHighlightBackground", (self.secondary_accent1, 0x77))?;
+        write_scope(f, "peekViewResult.selectionBackground", (self.primary_accent, 0x33))?;
         write_scope(f, "peekViewResult.selectionForeground", self.fg)?;
         write_scope(f, "peekViewTitle.background", self.darker_bg)?;
-        write_scope(f, "peekViewTitleDescription.foreground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "peekViewTitleDescription.foreground", (self.primary_accent, 0x55))?;
         write_scope(f, "peekViewTitleLabel.foreground", self.fg)?;
         write_scope(f, "problemsErrorIcon.foreground", self.red)?;
         write_scope(f, "problemsWarningIcon.foreground", self.orange)?;
-        write_scope(f, "rust_analyzer.inlayHint", Rgba { rgb: self.primary_accent, a: 0x55 })?;
-        write_scope(f, "scrollbarSlider.background", Rgba { rgb: self.primary_accent, a: 0x22 })?;
-        write_scope(f, "selection.background", Rgba { rgb: self.primary_accent, a: 0x44 })?;
+        write_scope(f, "rust_analyzer.inlayHint", (self.primary_accent, 0x55))?;
+        write_scope(f, "scrollbarSlider.background", (self.primary_accent, 0x22))?;
+        write_scope(f, "selection.background", (self.primary_accent, 0x44))?;
         write_scope(f, "settings.headerForeground", self.fg)?;
         write_scope(f, "sideBar.background", self.darker_bg)?;
-        write_scope(f, "sideBar.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
-        write_scope(f, "sideBarSectionHeader.background", Rgba { rgb: self.primary_accent, a: 0x11 })?;
+        write_scope(f, "sideBar.border", (self.primary_accent, 0x14))?;
+        write_scope(f, "sideBarSectionHeader.background", (self.primary_accent, 0x11))?;
         write_scope(f, "statusBar.background", self.darker_bg)?;
-        write_scope(f, "statusBar.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "statusBar.border", (self.primary_accent, 0x14))?;
         write_scope(f, "statusBar.debuggingBackground", self.darker_bg)?;
-        write_scope(f, "statusBar.foreground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "statusBar.foreground", (self.primary_accent, 0x55))?;
         write_scope(f, "statusBar.noFolderBackground", self.darker_bg)?;
         write_scope(f, "tab.activeBackground", self.bg)?;
         write_scope(f, "tab.activeForeground", self.fg)?;
-        write_scope(f, "tab.border", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "tab.border", (self.primary_accent, 0x14))?;
         write_scope(f, "tab.inactiveBackground", self.darker_bg)?;
-        write_scope(f, "tab.inactiveForeground", Rgba { rgb: self.primary_accent, a: 0x55 })?;
+        write_scope(f, "tab.inactiveForeground", (self.primary_accent, 0x55))?;
         write_scope(f, "terminal.ansiBlack", self.bg)?;
         write_scope(f, "terminal.ansiBlue", self.secondary_accent1)?;
         write_scope(f, "terminal.ansiBrightBlack", self.faded)?;
@@ -167,7 +167,7 @@ impl Theme {
         write_scope(f, "textPreformat.foreground", self.secondary_accent2)?;
         write_scope(f, "titleBar.activeBackground", self.darker_bg)?;
         write_scope(f, "titleBar.inactiveBackground", self.darker_bg)?;
-        write_scope(f, "window.activeBorder", Rgba { rgb: self.primary_accent, a: 0x14 })?;
+        write_scope(f, "window.activeBorder", (self.primary_accent, 0x14))?;
 
         writeln!(f, "\t}}")?;
 
@@ -240,40 +240,30 @@ impl fmt::Display for FontStyle {
     }
 }
 
-enum Color {
-    Rgba(Rgba),
-    Rgb(Rgb),
-}
-
-impl From<Rgba> for Color {
-    fn from(rgba: Rgba) -> Self {
-        Self::Rgba(rgba)
-    }
+struct Color {
+    rgb: Rgb,
+    a: Option<u8>,
 }
 
 impl From<Rgb> for Color {
     fn from(rgb: Rgb) -> Self {
-        Self::Rgb(rgb)
+        Self { rgb, a: None }
+    }
+}
+
+impl From<(Rgb, u8)> for Color {
+    fn from((rgb, a): (Rgb, u8)) -> Self {
+        Self { rgb, a: Some(a) }
     }
 }
 
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Rgba(rgba) => write!(f, "{}", rgba),
-            Self::Rgb(rgb) => write!(f, "{}", rgb),
+        if let Some(a) = self.a {
+            write!(f, "\"#{:06X}{:02X}\"", self.rgb.0, a)
+        } else {
+            write!(f, "{}", self.rgb)
         }
-    }
-}
-
-struct Rgba {
-    rgb: Rgb,
-    a: u8,
-}
-
-impl fmt::Display for Rgba {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "\"#{:06X}{:02X}\"", self.rgb.0, self.a)
     }
 }
 
