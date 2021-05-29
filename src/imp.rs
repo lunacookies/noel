9e5f98314ad076910dc80d6067525aca39946ad4
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-05-29 16:51:11 +1000

    Set tab colours

--- src/imp.rs
+++ src/imp.rs
@@ -15,6 +15,11 @@ fn workspace_colors(builder: &mut ThemeBuilder, scheme: &impl Scheme) {
     builder.add_workspace_rules(&["activityBar.background", "sideBar.background"], scheme.base(BaseScale::DarkBg));
     builder.add_workspace_rule("activityBar.foreground", scheme.base(BaseScale::Fg));
     builder.add_workspace_rule("activityBar.inactiveForeground", scheme.base(BaseScale::Faded));
+
+    builder.add_workspace_rules(&["tab.inactiveBackground", "tab.border"], scheme.base(BaseScale::DarkBg));
+    builder.add_workspace_rule("tab.activeForeground", scheme.base(BaseScale::Fg));
+    builder.add_workspace_rule("tab.inactiveForeground", scheme.base(BaseScale::Faded));
+    builder.add_workspace_rules(&["editorGroupHeader.tabsBackground", "editorGroupHeader.noTabsBackground"], scheme.base(BaseScale::DarkBg));
 }
 
 fn syntax_highlighting(builder: &mut ThemeBuilder, scheme: &impl Scheme) {
